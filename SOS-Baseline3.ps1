e24b87b996b4b18379661110f981863363250514
MITS <Billu@advancestuff.com>
2025-03-31 16:33:48 -0400

    Updated Write-Delayed to support transcript logging better.

--- SOS-Baseline3.ps1
+++ SOS-Baseline3.ps1
@@ -1,6 +1,6 @@
 ############################################################################################################
 #                                     SOS - New Workstation Baseline Script                                #
-#                                                 Version 1.5.0                                            #
+#                                                 Version 1.5.1                                            #
 ############################################################################################################
 #region Synopsis
 <#
@@ -22,7 +22,7 @@
     This script does not accept parameters.
 
 .NOTES
-    Version:        1.5.0
+    Version:        1.5.1
     Author:         Bill Ulrich
     Creation Date:  3/25/2025
     Requires:       Administrator privileges
@@ -46,7 +46,7 @@ if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 
 # Initial setup and version
 Set-ExecutionPolicy RemoteSigned -Force *> $null
-$ScriptVersion = "1.5.0"
+$ScriptVersion = "1.5.1"
 $ErrorActionPreference = 'SilentlyContinue'
 $WarningPreference = 'SilentlyContinue'
 $TempFolder = "C:\temp"
