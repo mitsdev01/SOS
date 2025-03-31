6043814758ea9a92273f0bc90be06d3097cab89b
MITS <Billu@advancestuff.com>
2025-03-31 14:13:51 -0400

    Updated domain join script.

--- SOS-Baseline.ps1
+++ SOS-Baseline.ps1
@@ -1,6 +1,6 @@
 ############################################################################################################
 #                                     SOS - New Workstation Baseline Script                                #
-#                                                 Version 1.4.3                                            #
+#                                                 Version 1.4.4                                            #
 ############################################################################################################
 #region Synopsis
 <#
@@ -22,7 +22,7 @@
     This script does not accept parameters.
 
 .NOTES
-    Version:        1.4.3
+    Version:        1.4.4
     Author:         Bill Ulrich
     Creation Date:  3/25/2025
     Requires:       Administrator privileges
@@ -46,7 +46,7 @@ if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 
 # Initial setup and version
 Set-ExecutionPolicy RemoteSigned -Force *> $null
-$ScriptVersion = "1.4.3"
+$ScriptVersion = "1.4.4"
 $ErrorActionPreference = 'SilentlyContinue'
 $WarningPreference = 'SilentlyContinue'
 $TempFolder = "C:\temp"
