5ff764ff4559876ec13e2826392549b0bce568db
MITS <Billu@advancestuff.com>
2025-04-01 11:50:37 -0400

    Updated version number

--- SOS-Baseline3.ps1
+++ SOS-Baseline3.ps1
@@ -46,7 +46,7 @@ if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 
 # Initial setup and version
 Set-ExecutionPolicy RemoteSigned -Force *> $null
-$ScriptVersion = "1.5.4d"
+$ScriptVersion = "1.5.4e"
 $ErrorActionPreference = 'SilentlyContinue'
 $WarningPreference = 'SilentlyContinue'
 $TempFolder = "C:\temp"
