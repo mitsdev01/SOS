56cfe1a0ac86ec3a6a0746837ff449af9d0a2aef
MITS <Billu@advancestuff.com>
2025-04-10 18:07:31 -0400

    updated version

--- SOS-Baseline.ps1
+++ SOS-Baseline.ps1
@@ -46,7 +46,7 @@ if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 }
 
 # Initial setup and version
-$ScriptVersion = "1.6.9g"
+$ScriptVersion = "1.6.9h"
 $ScriptVersion
 $ErrorActionPreference = 'SilentlyContinue'
 $WarningPreference = 'SilentlyContinue'
