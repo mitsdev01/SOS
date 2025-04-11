3abbad5fdf073038a3e130910ab51c882108681e
MITS <Billu@advancestuff.com>
2025-04-11 09:15:41 -0400

    Updated version

--- SOS-Baseline.ps1
+++ SOS-Baseline.ps1
@@ -46,7 +46,7 @@ if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 }
 
 # Initial setup and version
-$ScriptVersion = "1.7.0b"
+$ScriptVersion = "1.7.0c"
 $ErrorActionPreference = 'SilentlyContinue'
 $WarningPreference = 'SilentlyContinue'
 $TempFolder = "C:\temp"
