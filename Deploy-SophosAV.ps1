513fce4097fbb180d5881053197f35a81e843f62
mitsdev01 <mitsadmin@mitsdev.com>
2025-04-24 16:46:52 -0400

    Update Deploy-SophosAV.ps1

--- Deploy-SophosAV.ps1
+++ Deploy-SophosAV.ps1
@@ -43,6 +43,16 @@ Add-Type -AssemblyName System.Windows.Forms
 $ScriptVersion = "1.0.8"
 $ProgressPreference = 'SilentlyContinue'
 
+# Hide PowerShell Console Window
+Add-Type -Name Window -Namespace Console -MemberDefinition '
+[DllImport("Kernel32.dll")]
+public static extern IntPtr GetConsoleWindow();
+[DllImport("user32.dll")]
+public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
+'
+$consolePtr = [Console.Window]::GetConsoleWindow()
+[Console.Window]::ShowWindow($consolePtr, 0)
+
 # Function to decrypt and load installer links
 function Get-InstallerLinks {
     param (
